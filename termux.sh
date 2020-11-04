fba3ebcf3379a9c313dc51e81e1d6a4f7b6771fc
Elnur Babayev <35178745+semkir216@users.noreply.github.com>
2020-11-05 00:08:07 +0400

    Delete termux.sh

--- termux.sh
+++
@@ -1,9 +0,0 @@
-echo "Gerekli moduller yukleniyor"
-pkg install python
-pkg install python3
-pkg install python3-pip
-pip install requests
-pip install colorama
-echo "Moduller yuklendi."
-python3 bomber.py
-
